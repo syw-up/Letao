0d9332b289741a7d859303bcfa0ef7b2144a3b52
syw-up <1477481992S@.com>
2018-03-04 22:03:18 +0800

    完成首页功能

--- public/letao-pc/js/common.js
+++ public/letao-pc/js/common.js
@@ -15,9 +15,7 @@
        });
     
        //验证功能
-     
        if(window.location.href.indexOf('login.html') == -1){
-   
         $.ajax({
           type:'get',
           url:'/employee/checkRootLogin',
@@ -25,19 +23,47 @@
           success:function (info) { 
             console.log(info);  
             if(info.error == 400){
-              window.location.href='login.html'
+              // window.location.href='login.html'
             }
             
            }
         })
        }
+       
+       //点击显示二级分类
+       $('.nav li').eq(1).on('click',function () { 
+      
+           $(this).children('.second_ul').slideToggle();
+        })
 
+       //topbar点击功能
+       $('.topbar a').eq(0).on('click',function () { 
+        $('.aside').toggleClass('now');
+        $('.aside').hasClass('now')?$('.main').css('paddingLeft',0):$('.main').css('paddingLeft','180px')
+          
+        })
+       //点击退出功能
+       $('.topbar a').eq(1).on('click',function () { 
+        $('.modal').modal('show');
 
+       });
 
-
-
-
-
+        $('.sure_modal .modal-footer button').eq(1).on('click',function () { 
+          $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            success:function (info) { 
+               if(info.success){
+                 window.location.href='login.html';
+                  $('.modal').modal('hide');
+                
+               }
+             
+             }
+          })
+          
+         })
 
 
 
