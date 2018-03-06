1c491183ff30d1a89dcfc4f72ac8c9c43e26c607
syw-up <1477481992S@.com>
2018-03-06 11:37:21 +0800

    pc用户完成

--- public/letao-pc/js/user.js
+++ public/letao-pc/js/user.js
@@ -12,13 +12,52 @@
               },
               dataType:'json',
               success:function (info) { 
-                  console.log(info);
+                 console.log(info);
+                 
                   var str=template('user-tmp',info);
-                  $('tbody').html(str);        
+                  $('tbody').html(str);    
+                  $('#pagenator').bootstrapPaginator({
+                    bootstrapMajorVersion:3,
+                    currentPage:1,
+                    numberOfPages:5,
+                    totalPages:Math.ceil(info.total/info.size),
+                    onPageClicked:function (a,b,c,p) { 
+                        
+                     }      
+                  
+                   });    
                }
           })
          }
        render();
+       //点击模态框事件
+       var  id;
+       var  isDelete;
+       $('.main_content tbody ').on('click','button',function () {  
+         $('.modal').modal('show');
+         id=$(this).parent().data('id');
+
+          if($(this).hasClass('btn-success')){
+              isDelete=1;
+            }else{
+              isDelete=0;
+         }    
+         
+       })
+       
+       //确认功能
+       $('.btn_sure').on('click',function () {      
+           $.post('/user/updateUser',{
+               id:id,
+               isDelete:isDelete
+           },function (info) { 
+                render();
+                $('.modal').modal('hide');
+               
+            })
+        })
+
+ 
 
 
 
